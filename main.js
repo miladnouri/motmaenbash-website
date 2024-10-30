396c39a2c2670280b5815498a00a0c20a3a6ce87
Milad <milad.nu@gmail.com>
2024-10-30 19:49:37 +0330

    Links bug fix Tiny improvements

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(e) {
   $("header ul li a,header a.btn-bg").click(function(e) {
-    e.preventDefault();
     const section = $(this).attr("href");
-    
+
     if (section.startsWith('#')) {
       e.preventDefault();  // Only prevent default action for internal links
       $("html").animate({ scrollTop: $(section).offset().top }, 500);
