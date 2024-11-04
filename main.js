2708c0a1a67ac3a4d99b0b2edb5cfaa4d20a1a6a
Milad <milad.nu@gmail.com>
2024-11-04 12:16:15 +0330

    Add App Section

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e) {
-  $("header ul li a,header a.btn-bg").click(function(e) {
+  $("header ul li a,header a.btn").click(function(e) {
     const section = $(this).attr("href");
 
     if (section.startsWith('#')) {
@@ -9,18 +9,4 @@ $(document).ready(function(e) {
     
   });
 
-  $(window).scroll(function(e) {
-    if ($(window).scrollTop() >= $("header").outerHeight() / 2) {
-      if (
-        $(window).scrollTop() + $(window).height() >=
-        $(document).height() - 150
-      ) {
-        $("#hover-btn").fadeOut(100);
-      } else {
-        $("#hover-btn").fadeIn(100);
-      }
-    } else {
-      $("#hover-btn").fadeOut(100);
-    }
-  });
 });
